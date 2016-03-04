cb6c6ee1b4f9c6999ae37aa10b4c93b91862d7d9
Chris Young <ctopheryoung@gmail.com>
2016-03-04 09:28:09 -0800

    configure band_venues database for tests and production

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -4,13 +4,15 @@ import org.sql2o.*;
 public class DatabaseRule extends ExternalResource {
 
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/DATABASENAME_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/band_venues_test", null, null);
    }
 
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM nameoftableyouwanttodelete *;";
-      con.createQuery(sql).executeUpdate();
+      String venuesDeleteQuery = "DELETE FROM bands *";
+      String bandsDeleteQuery = "DELETE FROM bands *";
+      con.createQuery(venuesDeleteQuery).executeUpdate();
+      con.createQuery(bandsDeleteQuery).executeUpdate();
     }
   }
 }
