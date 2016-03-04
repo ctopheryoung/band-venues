ff5748a85eea12c827f667cd3397955a2ed1affa
Chris Young <ctopheryoung@gmail.com>
2016-03-04 09:53:12 -0800

    create Band and Venue classes, test files

---
+++ src/main/java/Band.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Band {
+  private int id;
+  private String name;
+
+  //CONSTRUCTOR
+  public Band(String name) {
+    this.name = name;
+  }
+}
---
+++ src/main/java/Venue.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Venue {
+  private int id;
+  private String name;
+
+  //CONSTRUCTOR
+  public Venue(String name) {
+    this.name = name;
+  }
+}
