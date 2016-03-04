fdd93539683028bac19bf9c9ba75ec240d625803
Chris Young <ctopheryoung@gmail.com>
2016-03-04 10:15:59 -0800

    write tests for all() method

--- src/main/java/Band.java
+++ src/main/java/Band.java
@@ -9,4 +9,24 @@ public class Band {
   public Band(String name) {
     this.name = name;
   }
+
+  //OVERRIDE FOR EQUALS METHOD
+  @Override
+  public boolean equals(Object otherBand){
+    if (!(otherBand instanceof Band)) {
+      return false;
+    } else {
+      Band newBand = (Band) otherBand;
+      return this.getName().equals(newBand.getName());
+    }
+  }
+
+  //GETTER METHODS
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
 }
--- src/test/java/BandTest.java
+++ src/test/java/BandTest.java
@@ -7,6 +7,113 @@ public class BandTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
-  //Tests go here
+  @Test
+  public void band_instantiatesCorrectly_true() {
+    Band testBand = new Band("Fruition");
+    assertEquals(true, testBand instanceof Band);
+    assertEquals("Fruition", testBand.getName());
+    assertEquals(Band.all().size(), testBand.getId());
+  }
 
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Band.all().size(), 0);
+  }
+
+  // @Test
+  // public void equals_returnsTrueIfNamesAreTheSame() {
+  //   Band firstBand = new Band("Onions");
+  //   Band secondBand = new Band("Onions");
+  //   assertTrue(firstBand.equals(secondBand));
+  // }
+  //
+  // @Test
+  // public void equals_returnsTrueIfSavedBandsAretheSame() {
+  //   Band testBand = new Band("Fruition");
+  //   testBand.save();
+  //   assertTrue(Band.all().get(0).equals(testBand));
+  // }
+  //
+  // @Test
+  // public void save_assignsIdToObject() {
+  //   Band testBand = new Band("Fruition");
+  //   testBand.save();
+  //   Band savedBand = Band.all().get(0);
+  //   assertEquals(testBand.getId(), savedBand.getId());
+  // }
+  //
+  // @Test
+  // public void save_savesIntoDatabase_true() {
+  //   Band testBand = new Band("Fruition");
+  //   testBand.save();
+  //   assertEquals(Band.all().get(0).getName(), "Fruition");
+  // }
+  //
+  // @Test
+  // public void find_findsBandInDatabase_true() {
+  //   Band testBand = new Band("Fruition");
+  //   testBand.save();
+  //   Band savedBand = Band.find(testBand.getId());
+  //   assertEquals(savedBand.getName(), "Fruition");
+  // }
+  //
+  // @Test
+  // public void all_returnsAllInstancesOfBand_true() {
+  //   Band firstBand = new Band("Fruition");
+  //   Band secondBand = new Band("Chili Powder");
+  //   firstBand.save();
+  //   secondBand.save();
+  //   assertTrue(Band.all().contains(firstBand));
+  //   assertTrue(Band.all().contains(secondBand));
+  // }
+  //
+  // @Test
+  // public void find_returnsBandWithSameId_secondBand() {
+  //   Band firstBand = new Band("Fruition");
+  //   Band secondBand = new Band("Chili Powder");
+  //   firstBand.save();
+  //   secondBand.save();
+  //   assertEquals(Band.find(secondBand.getId()), secondBand);
+  // }
+  //
+  // @Test
+  // public void find_returnsNullWhenNoBandFound_null() {
+  //   assertTrue(Band.find(999) == null);
+  // }
+  //
+  // @Test
+  // public void delete_deleteDeletesBand() {
+  //   Band myBand = new Band("Fruition");
+  //   myBand.save();
+  //   myBand.delete();
+  //   assertEquals(Band.all().size(), 0);
+  // }
+  //TESTS TO BE USED AFTER RECIPE CLASS IS SET UP
+  // @Test
+  // public void addRecipe_addsRecipeForIngredient() {
+  //   Recipe myRecipe = new Recipe(????);
+  //   myRecipe.save();
+  //
+  //   Ingredient myIngredient = new Ingredient("Fruition");
+  //   myIngredient.save();
+  //
+  //   myIngredient.addRecipe(myRecipe);
+
+  //   Recipe savedRecipe = myIngredient.getRecipes().get(0);
+  //   assertTrue(myRecipe.equals(savedRecipe));
+  // }
+  //
+  // @Test
+  // public void getRecipes_returnsRecipesThatUseIngredient_List() {
+  //   Recipe myRecipe = new Recipe(????);
+  //   myRecipe.save();
+  //
+  //   Ingredient myIngredient = new Ingredient("Fruition");
+  //   myIngredient.save();
+  //
+  //   myIngredient.addRecipe(myRecipe);
+
+  //   List<Recipe> savedRecipes = myIngredient.getRecipes();
+  //   assertEquals(savedRecipe.size(), 1);
+  // }
 }
